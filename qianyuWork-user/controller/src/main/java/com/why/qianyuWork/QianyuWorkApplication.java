f34d8c6366b1bf4477b472f1f8a3d692ccf8aa73
why <488009667@qq.com>
2021-10-20 12:34:33 +0800

    用户工程基础环境搭建

---
+++ qianyuWork-user/controller/src/main/java/com/why/qianyuWork/QianyuWorkApplication.java
@@ -0,0 +1,17 @@
+package com.why.qianyuWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName：QianyuWorkApplication
+ * @Description：todo springboot启动类
+ * @Author: why
+ * @DateTime：2021/10/20 12:27
+ */
+@SpringBootApplication
+public class QianyuWorkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QianyuWorkApplication.class,args);
+    }
+}
